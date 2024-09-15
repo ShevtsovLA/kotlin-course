593bf9027911e8dd8836b1b0208029570a9b3dc6
Лев <carbajot@gmail.com>
2024-09-15 18:32:33 +0300

    Initial commit

--- src/main/kotlin/org/shevtsovla/kotlincourse/Main.kt
+++ src/main/kotlin/org/shevtsovla/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-             package org.shevtsovla
+             package org.shevtsovla.org.shevtsovla.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,4 +13,57 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
-}something
+}
+             val name: String ="Alice"
+
+var age: Int = 28
+
+const val G: Double = 9.98
+
+lateinit var userInfo: String
+
+val lazyValue: String by lazy {
+    "What's goin' on?"
+}
+var speed: Double? = null
+
+var count: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) field = value
+    }
+
+val chassis: String = "12345abc"
+
+var colour: String = "Purple"
+
+var distance: Double = 0.0
+
+lateinit var owner: String
+
+const val WHEELS: Int = 4
+
+val report: String by lazy {
+    ""
+}
+
+var fuel: Double = 0.0
+    get() = field
+    set(value) {
+        if (value > 0) field += value
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/org/shevtsovla/kotlincourse/lesson4/Homework.kt
@@ -0,0 +1,81 @@
+package org.shevtsovla.kotlincourse.lesson4
+
+const val ANSWER: Int = 42
+
+var number1: Long = 98765432123456789L
+
+var time: Float = 23.45f
+
+var number2: Double = 0.123456789
+
+val topLanguages: String = "Kotlin & Java"
+
+val earthIsFlat: Boolean = false
+
+var spanishYes: Char = 'c'
+
+val cash: Int = 500
+
+val number3: Long = 4294967296L
+
+val number4: Float = 18.0f
+
+val number5: Double = -0.001
+
+val chatGPT: String = "OpenAI"
+
+var trap1: Boolean = true
+
+var trap2: Int = 9
+
+var lastYearOnEarth: Int = 2048
+
+val number6: Long = 10000000000L
+
+val number7: Float = 5.75f
+
+val number8: Double = 1.414
+
+val skynet: String = "Artificial Intelligence"
+
+val iAmWoman: Boolean = false
+
+var trap3: String = "@"
+
+val number9: Int = 1024
+
+val numer10: Long = 1234567890123L
+
+val number11: Float = 10.01f
+
+val number12: Double = -273.15
+
+val rocket: String = "SpaceX"
+
+val easyHomework: Boolean = true
+
+val myBrain: String = "\uD83E\uDD2F"
+
+val trap4: Int = 65535
+
+var number13: Long = 72057594037927935L
+
+var number14: Float = 2.71828f
+
+var number15: Double = 101.0101
+
+val future: String = "Quantum Computing"
+
+var nePravda: Boolean = false
+
+val extwitter: Char = 'x'
+
+val pi100: Int = 314
+
+var number16: Long = 123456789123456789L
+
+var number17: Float = 6.626f
+
+val lilBond: Double = 0.007
+
+val memoryEater: String = "Android Studio"
